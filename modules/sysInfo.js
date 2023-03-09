ca277df07d02ca0670e35b8f33ab61604068394f
Jonas Schneider <schneider-jonas@mein.gmx>
2023-03-09 22:40:05 +0100

    save

--- modules/sysInfo.js
+++ modules/sysInfo.js
@@ -2,8 +2,8 @@ import osu from "node-os-utils";
 
 global.IO.on("connect", (socket) => {
     socket.on("get:sysInfo", async (ack) => {
-        let cpu = await osu.cpu.usage();
-        let mem = await osu.mem.info()
-        ack({cpu, mem: {total: mem.totalMemMb, used: mem.usedMemMb}})
+        let cpu = (await osu.cpu.usage()).toFixed(2);
+        let mem = await osu.mem.info();
+        ack({cpu, mem: {total: (mem.totalMemMb/1024).toFixed(1), used: (mem.usedMemMb/1024).toFixed(1)}});
     })
 });
