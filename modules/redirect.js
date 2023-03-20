55ac2a2ec89a3d545d7a1d083a70ac8c80a58164
Jonas Schneider <schneider-jonas@mein.gmx>
2023-03-20 23:37:29 +0100

    save

--- modules/redirect.js
+++ modules/redirect.js
@@ -1,17 +1,28 @@
 import { portIsUnused, domainIsUnused, nameIsUnused } from "../wrapper/entities.js";
 
-global.SE.on("redirect:create", async (data, ack) => {
+global.SE.on("redirect:write", async (data, ack) => {
     if (!data?.name || !data?.network?.sub || !data?.network?.domain || !data?.network?.port) {
         ack({ error: true, msg: "Input data incomplete" });
         return;
     }
+
+    //delete old entity by _id
+    let target;
+    if (data?._id) {
+        target = global.ENTITIES.findOne({ _id: data._id });
+        global.ENTITIES.deleteOne(target);
+    }
+
+    //name is unused
     if (await nameIsUnused(data.name)) {
+        //port and domain unused
         if (await portIsUnused(data.network.port)) {
             if (await domainIsUnused(data.network.sub, data.network.domain)) {
-                //port and domain unused
-                global.ENTITIES.insertOne({ type: "redirect", ...data, active: true });
+                //insert new entity
+                global.ENTITIES.insertOne({ type: "redirect", ...data });
                 //TODO: reload proxy
                 ack({ error: false, msg: "Redirect successfully created" });
+                return;
             } else {
                 ack({ error: true, msg: "Domain already used" });
             }
@@ -21,10 +32,14 @@ global.SE.on("redirect:create", async (data, ack) => {
     } else {
         ack({ error: true, msg: "Name already used" });
     }
+
+    //insert old entity back if something goes wrong
+    if(target) global.ENTITIES.insertOne(target);
 });
 
 global.SE.on("redirect:list", (ack) => {
     let redirects = global.ENTITIES.findMany({ type: "redirect" });
+    redirects.sort((a, b) => a.name < b.name ? -1 : 1);
     ack({ error: false, msg: "Fetched all redirect entities", payload: redirects });
 });
 
