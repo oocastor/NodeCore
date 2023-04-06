e0c8531e94e1413ec5699409538a1eb585c65262
Jonas Schneider <schneider-jonas@mein.gmx>
2023-04-07 01:21:27 +0200

    save

--- modules/utils.js
+++ modules/utils.js
@@ -1,8 +1,24 @@
 import { getUnusedPort } from "../wrapper/entities.js";
 
+global.SE.on("domain:add", async (data, ack) => {
+    let { domain } = data;
+    let { value } = await global.STORAGE.findOne({ entity: "domains" });
+    value.push(domain);
+    await global.STORAGE.updateOne({ entity: "domains" }, { value });
+    ack({error: false, msg: "New domain successfully added", payload: null})
+})
+
+global.SE.on("domain:delete", async (data, ack) => {
+    let { domain } = data;
+    let { value }= await global.STORAGE.findOne({ entity: "domains" });
+    value = value.filter(f => f !== domain);
+    await global.STORAGE.updateOne({ entity: "domains" }, { value });
+    ack({error: false, msg: "Domain successfully deleted", payload: null})
+})
+
 global.SE.on("domain:list", async (ack) => {
-    let domains = await global.STORAGE.findOne({ entity: "domains" }).value;
-    ack({error: false, msg: "Fetched available domains", payload: domains})
+    let { value } = await global.STORAGE.findOne({ entity: "domains" });
+    ack({error: false, msg: "Fetched available domains", payload: value})
 })
 
 global.SE.on("port:get", async (ack) => {
