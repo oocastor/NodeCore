1eaba093605e792bd7a21fd5afabb2a5d61876e9
Jonas Schneider <schneider-jonas@mein.gmx>
2023-06-02 14:03:05 +0200

    added gui

--- bin/web.js
+++ bin/web.js
@@ -4,7 +4,7 @@ import express from "express";
 const app = express();
 const https = createServer(app);
 
-app.use(express.static('public'));
+app.use(express.static('gui/dist'));
 
 const listener = https.listen(1001, () => {
     console.log(`web/socket server running on port ${listener.address().port}`)
