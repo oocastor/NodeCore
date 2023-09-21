c51e57099377d62d2111bc74c5ff81f90aa9774d
Der Schöpfer <marius@mauer.dev>
2023-09-21 12:24:15 +0000

    Connection fix, block for undefinded access

--- bin/proxy.js
+++ bin/proxy.js
@@ -6,10 +6,7 @@ import { ensureDirSync } from "fs-extra";
 import httpProxy from "http-proxy";
 import Borgoose from "borgoose";
 import cluster from "cluster";
-// import tls from 'tls';
 
-// // Liste der verfügbaren SSL/TLS-Versionen anzeigen
-// console.log('Verfügbare SSL/TLS-Versionen:', tls.getCiphers());
 //** HTTP */
 const app = express();
 const _http = http.createServer(app);
@@ -18,9 +15,9 @@ let ACME = []
 process.on("message", (data) => {
     data = JSON.parse(data);
     if (data.event == "updateACME") {
-        if(data.data.add) {
+        if (data.data.add) {
             ACME.push(data.data.add);
-        } else if(data.data.delete) {
+        } else if (data.data.delete) {
             ACME = ACME.filter(o => o.token != data.data.delete.token);
         }
     }
@@ -43,7 +40,7 @@ app.get("/.well-known/acme-challenge/:id", (req, res) => {
     let id = req.params.id;
     let target = ACME.find(f => f.token == id);
 
-    if(target == undefined) {
+    if (target == undefined) {
         res.send("Nothing to see here!");
         return;
     }
@@ -87,18 +84,21 @@ function createHttpsServer() {
     _https = https.createServer(certs[0].data, (req, res) => {
         let ip = req.socket.remoteAddress;
         let target = getTargetByDomain(req.headers.host);
-        if(target !== "undefined"){
+        if (target !== "undefined") {
             global.log.success(`redirect ${convertIPv6MappedToIPv4(ip)} to ${target} (${req.headers.host})`);
-        }else{
-            global.log.warn(`redirect ${convertIPv6MappedToIPv4(ip)} to ${target} (${req.headers.host})`);
+            proxy.web(req, res, { target });
+        } else {
+            global.log.warn(`blocked redirect ${convertIPv6MappedToIPv4(ip)} to ${target} (${req.headers.host})`);
+            //IPs tracken?
+            res.statusCode = 404;
         }
-        proxy.web(req, res, { target });
     });
 
     certs.slice(1).forEach(o => {
         o.altNames.forEach(alt => _https.addContext(alt, o.data));
     });
     proxy.on("error", function (err, req, res) {
+        global.log.info(req.headers)
         global.log.error('Proxy', err);
         global.log2File.error(err)
         res.statusCode = 500;
@@ -132,6 +132,7 @@ function updateHttpsServer() {
 }
 
 function getTargetByDomain(host) {
+    if (!host) return "undefined"; 
     const ENTITIES = new Borgoose('data/entities.json', { syncOnWrite: true, createWithId: true });
 
     let address = host.split(".");
@@ -145,13 +146,9 @@ function getTargetByDomain(host) {
         found = ENTITIES.findOne({ type: "instance", network: { redirect: { domain: `${address[1]}.${address[2]}`, sub: address[0] } } }) ||
             ENTITIES.findOne({ type: "redirect", network: { domain: `${address[1]}.${address[2]}`, sub: address[0] }, status: 1 });
     }
-
     if (!found) return "undefined"; //force an error
 
     let port = found.network?.redirect?.port || found.network.port;
-
-    //redirect NodeCore to https
-    if(port == process.env.PORT) return `https://localhost:${port}`
 
     return `http://localhost:${port}`
 }
@@ -159,14 +156,14 @@ function getTargetByDomain(host) {
 function convertIPv6MappedToIPv4(ipv6Mapped) {
     // Die IPv6-Mapped-IPv4-Adresse beginnt mit '::ffff:'.
     const prefix = '::ffff:';
-    
+
     // Prüfen, ob die gegebene Adresse mit '::ffff:' beginnt.
     if (ipv6Mapped.startsWith(prefix)) {
-      // Die IPv4-Adresse ist der Teil der Adresse nach dem Prefix '::ffff:'.
-      const ipv4Address = ipv6Mapped.substr(prefix.length);
-      return ipv4Address;
+        // Die IPv4-Adresse ist der Teil der Adresse nach dem Prefix '::ffff:'.
+        const ipv4Address = ipv6Mapped.substr(prefix.length);
+        return ipv4Address;
     } else {
-      // Falls die Adresse nicht mit '::ffff:' beginnt, ist es bereits eine normale IPv4-Adresse.
-      return ipv6Mapped;
+        // Falls die Adresse nicht mit '::ffff:' beginnt, ist es bereits eine normale IPv4-Adresse.
+        return ipv6Mapped;
     }
-  }
+}
