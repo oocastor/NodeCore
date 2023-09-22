54c3f768ada0c9424f1aebeb9c30f57f2d74665a
Der Schöpfer <marius@mauer.dev>
2023-09-22 12:25:28 +0000

    npm install rekursiv. NuxtJS works. yippi

--- helper/instance.helper.js
+++ helper/instance.helper.js
@@ -1,5 +1,6 @@
 import fs from "fs";
 import { spawn } from 'child_process';
+import path from "path"
 function getPackageJSON(instance) {
     let path = global.CONFIG.findOne({ entity: "path" }).value;
 
@@ -11,31 +12,50 @@ function getPackageJSON(instance) {
     }
 }
 const checkAndInstallModules = (instance) => {
-    let path = global.CONFIG.findOne({ entity: "path" }).value+"/"+instance._id;
-    return new Promise((resolve, reject) => {
-      if (!fs.existsSync(`${path}/node_modules`)) {
-        const install = spawn('npm', ['install'], { cwd: path, stdio: 'ignore' });
-  
-        install.on('close', (code) => {
+  let basePath = global.CONFIG.findOne({ entity: "path" }).value+"/"+instance._id;
+  return new Promise((resolve, reject) => {
+    const walkDir = (dir) => {
+      fs.readdirSync(dir).forEach(file => {
+        const fullPath = path.join(dir, file);
+        if (file === 'node_modules') return; // Überspringe den node_modules Ordner
+        if (fs.lstatSync(fullPath).isDirectory()) {
+          walkDir(fullPath);
+        } else if (file === 'package.json') {
+          installNodeModules(dir).catch(err => reject(err));
+        }
+      });
+    };
+
+    const installNodeModules = (dir) => {
+      return new Promise((res, rej) => {
+        const install = spawn('npm', ['install'], { cwd: dir, stdio: 'ignore' });
+
+        install.on('close', code => {
           if (code !== 0) {
-            resolve({error: true, msg: `npm install failed with code ${code}`, payload: null})
+            rej({ error: true, msg: `npm install in ${dir} failed with code ${code}` });
           } else {
-            global.log.success('npm install done');
-            resolve({error: false, msg: `npm install done`, payload: null})
+            global.log.success(`npm install in ${dir} done`);
+            res({ error: false, msg: `npm install in ${dir} done` });
           }
         });
-  
-        install.on('error', (err) => {
-          global.log.error('Failed to start npm install', err);
-          global.log2File.error('Failed to start npm install', err);
-          resolve({error: true, msg: err, payload: null})
+
+        install.on('error', err => {
+          global.log.error(`Failed to start npm install in ${dir}`, err);
+          global.log2File.error(`Failed to start npm install in ${dir}`, err);
+          rej({ error: true, msg: err.toString() });
         });
-      } else {
-        global.log.info('node_modules already installed')
-        resolve({error: false, msg: `node_modules bereits vorhanden`, payload: null})
-      }
-    });
-  };
+      });
+    };
+
+    try {
+      walkDir(basePath);
+      resolve({ error: false, msg: 'All installations done' });
+    } catch (err) {
+      reject({ error: true, msg: err.toString() });
+    }
+  });
+};
+
 export {
     getPackageJSON, checkAndInstallModules
 }
