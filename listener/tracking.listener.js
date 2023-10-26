76faa2faaebec959102c46ddd1b447cfc3176448
Jonas Schneider <schneider-jonas@mein.gmx>
2023-10-26 12:42:40 +0200

    added lazy loading for tracking data endpoint

--- listener/tracking.listener.js
+++ listener/tracking.listener.js
@@ -15,8 +15,20 @@ global.SE.on("tracking:set", async (data, ack) => {
     ack({ error: false, msg: "Tracking settings changed", payload: null });
 });
 
-//TODO: lazy load data
-global.SE.on("tracking:data", (ack) => {
-    let data = global.TRACKING.findMany();
-    ack({ error: false, msg: "Fetched tracking data", payload: data });
+global.SE.on("tracking:data", (data, ack) => {
+    let { page, filter, sort } = data;
+    let totalTrackingData = global.TRACKING.findMany().reverse().sort((a, b) => !sort.ascending ? Date.parse(b.timestamp) - Date.parse(a.timestamp) : Date.parse(a.timestamp) - Date.parse(b.timestamp))
+        .filter(f => JSON.stringify(f).includes(filter.text)).filter(f => {
+            switch(filter.authorized) {
+                case 0:
+                    return true;
+                case 1:
+                    return f.authorized;
+                case 2:
+                    return !f.authorized;
+            }
+        });
+    let trackingData = totalTrackingData.slice(page * 20, (page + 1) * 20);
+    let totalRecords = totalTrackingData.length;
+    ack({ error: false, msg: "Fetched tracking data", payload: { trackingData, totalRecords } });
 });
